fac01259b18b4e3928e7a21beb5cbab3999f69ae
Arnold LAMBOU <lambouarnold@gmail.com>
2020-10-04 10:53:08 +0100

    Interfaces, and components exposed

---
+++ lib/index.ts
@@ -0,0 +1,18 @@
+import OauthClient from "./OauthClient";
+
+export { default as OauthClient } from "./OauthClient";
+export {
+  AuthorizationCodeGrantOptions,
+  AuthorizationCodePKCEGrantOptions,
+  ClientCredentialsGrantFuncTyle,
+  ClientCredentialsGrantOptions,
+  GetAuthorizationTokenFuncType,
+  GetAuthorizationUrlFuncType,
+  ImplicitGrantOptions,
+  OauthClientConfig,
+  OauthOptions,
+  PasswordGrantFuncType,
+  PasswordGrantOptions,
+  RequestOptions,
+  TokenResponse,
+} from "./interfaces";
---
+++ lib/interfaces/index.ts
@@ -0,0 +1,15 @@
+export type { default as AuthorizationCodeGrantOptions } from "./AuthorizationCodeGrantOptions";
+export type { default as AuthorizationCodePKCEGrantOptions } from "./AuthorizationCodePKCEGrantOptions";
+export type { default as ClientCredentialsGrantOptions } from "./ClientCredentialsGrantOptions";
+export type { default as PasswordGrantOptions } from "./PasswordGrantOptions";
+export type { default as ImplicitGrantOptions } from "./ImplicitGrantOptions";
+
+export type { default as ClientCredentialsGrantFuncTyle } from "./ClientCredentialsGrantFuncTyle";
+export type { default as GetAuthorizationTokenFuncType } from "./GetAuthorizationTokenFuncType";
+export type { default as GetAuthorizationUrlFuncType } from "./GetAuthorizationUrlFuncType";
+export type { default as PasswordGrantFuncType } from "./PasswordGrantFuncType";
+
+export type { default as OauthClientConfig } from "./OauthClientConfig";
+export type { default as OauthOptions } from "./OauthOptions";
+export type { default as RequestOptions } from "./RequestOptions";
+export type { default as TokenResponse } from "./TokenResponse";
