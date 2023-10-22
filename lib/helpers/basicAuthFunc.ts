843167db5e81d4093f7de040ee9dbef3d618fb9b
Arnold LAMBOU <lambouarnold@gmail.com>
2023-10-22 11:48:09 +0100

    updated base64 encryption logic for basic authentication

--- lib/helpers/basicAuthFunc.ts
+++ lib/helpers/basicAuthFunc.ts
@@ -1,3 +1,5 @@
+
+
 /**
  * Generate basic authentication header value
  * @param username username
@@ -7,5 +9,13 @@ export default function generateBasicAuthentication(
   username: string,
   password: string
 ) {
-  return `Basic ${Buffer.from(`${username}:${password}`).toString("base64")}`;
+  // we are in a node js environment
+  if (typeof Buffer !== 'undefined') {
+    return `Basic ${Buffer.from(`${username}:${password}`).toString("base64")}`;
+  } else if (typeof btoa === "function") {
+    // browser environemnt
+    return `Basic ${btoa(`${username}:${password}`)}`;
+  } else {
+    throw new Error('Unable to create the base 64 string.')
+  }
 }
